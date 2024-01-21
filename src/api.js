da721e8ad6a0903228269a5be44ca4e367fddad8
yerinra <yerinaleph@gmail.com>
2024-01-21 17:41:14 +0900

    ✨ Feat: API 연결

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 const API_ENDPOINT =
   "https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev";
 
-const request = async (url) => {
-  const data = await fetch(url);
-  return await data.json();
+export const fetchResult = async (keyword) => {
+  const res = await fetch(API_ENDPOINT + `/languages?keyword=${keyword}`);
+
+  if (!res.ok) throw new Error("Failed to Load.");
+
+  return res.json();
 };
