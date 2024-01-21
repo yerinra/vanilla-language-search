fd267f0172088a5c342134b3944ef84dfd9894ff
yerinra <yerinaleph@gmail.com>
2024-01-21 17:40:57 +0900

    ✨ Feat: SearchInput 컴포넌트 구현

---
+++ src/components/SearchInput.js
@@ -0,0 +1,38 @@
+export default function SearchInput({ $app, initialState, onFetch }) {
+  this.$inputSection = document.createElement("form");
+  this.$inputSection.className = "SearchInput";
+  $app.appendChild(this.$inputSection);
+
+  this.$input = document.createElement("input");
+  this.$input.className = "SearchInput__input";
+  this.$input.placeholder = "프로그램 언어를 입력하세요.";
+  this.$input.type = "text";
+  this.$inputSection.appendChild(this.$input);
+  this.$input.focus();
+  // this.$input.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  // });
+  this.$inputSection.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  this.state = initialState;
+  this.setState = (newState) => {
+    this.state = newState;
+  };
+
+  this.$input.addEventListener("keyup", (e) => {
+    const actionIgnoreKeys = [
+      "Enter",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+    ];
+
+    if (!actionIgnoreKeys.includes(e.key)) {
+      this.setState(e.target.value);
+      onFetch(this.state);
+    }
+  });
+}
